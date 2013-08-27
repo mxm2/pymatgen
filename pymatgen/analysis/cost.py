be16a8516c522baa2adaa7b08a15e6103cc8cb78
Anubhav Jain <ajain@lbl.gov>
2013-08-27 15:30:16 -0700

    convert db from kg->mol, allow cost/mol and cost/kg

--- pymatgen/analysis/cost.py
+++ pymatgen/analysis/cost.py
@@ -2,7 +2,8 @@ from collections import defaultdict
 import csv
 import os
 import itertools
-from pymatgen import Composition, singleton
+from pymatgen import Composition, singleton, AMU_TO_KG
+from pymatgen.core.physical_constants import AVOGADROS_CONST
 from pymatgen.matproj.snl import is_valid_bibtex
 from pymatgen.phasediagram.entries import PDEntry
 from pymatgen.phasediagram.pdanalyzer import PDAnalyzer
@@ -40,13 +41,16 @@ class CostDB(object):
         filename = os.path.join(os.path.dirname(__file__), "cost_db.csv")
         reader = csv.reader(open(filename, "rb"))
         for row in reader:
-            pde = CostEntry(row[0], float(row[1]), row[2], row[3])
-            comp = pde.composition.reduced_formula
+            comp = Composition.from_formula(row[0])
+            cost_per_mol = float(row[1]) * comp.weight * AMU_TO_KG * AVOGADROS_CONST
+            pde = CostEntry(comp.formula, cost_per_mol, row[2], row[3])
             chemsys = "-".join([el.symbol for el in pde.composition.elements])
             self._chemsys_entries[chemsys].append(pde)
-            self._comp_energies[comp] = min(pde.energy_per_atom, self._comp_energies.get(comp, float('inf')))
+            self._comp_energies[comp.reduced_formula] = min(pde.energy_per_atom, self._comp_energies.get(comp.reduced_formula, float('inf')))
 
-    def get_cost(self, composition):
+            print self._comp_energies
+
+    def get_cost_per_mol(self, composition):
         # first check if cost is explicitly defined for this composition
         if composition.reduced_formula in self._comp_energies:
             return self._comp_energies[composition.reduced_formula] * composition.num_atoms
@@ -62,10 +66,14 @@ class CostDB(object):
         pd = PhaseDiagram(entries_list)
         pda = PDAnalyzer(pd)
 
-        return sum(k.energy*v*composition.num_atoms for k, v in pda.get_decomposition(Composition.from_formula('LiFeO2')).iteritems())
+        return sum(k.energy*v*composition.num_atoms for k, v in pda.get_decomposition(composition).iteritems())
+
+    def get_cost_per_kg(self, comp):
+        return self.get_cost_per_mol(comp)/(comp.weight * AMU_TO_KG * AVOGADROS_CONST)
 
 
 
 if __name__ == "__main__":
     cdb = CostDB()
-    print cdb.get_cost(Composition.from_formula("LiFeO2"))
+    print cdb.get_cost_per_mol(Composition.from_formula("LiFeO2"))
+    print cdb.get_cost_per_kg(Composition.from_formula("LiFeO2"))
