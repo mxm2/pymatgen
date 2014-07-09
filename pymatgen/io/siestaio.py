f814c03e5d3ce253fd360a2d62eaafeae6fe9080
Artem Prihodko <ars21031960@gmail.com>
2014-07-10 02:30:03 +0400

    re-write parsing

--- pymatgen/io/siestaio.py
+++ pymatgen/io/siestaio.py
@@ -257,6 +257,46 @@ class SiestaOutput(object):
 		Args:
 			filename: path to Siesta output file.
 		"""
+
+		f_energy_patt = re.compile("Final energy (eV):", re.I | re.M)
+		qtot_patt = re.compile("Qtot\s*=\s*([\d.-]+)")
+		basisfun_patt = re.compile("nzeta=(\d+)\s+polorb=(\d+)")
+		press_patt = re.compile("([\d.]+)\s*([\d.]+)\s*Ry/Bohr\*\*3")
+		cell_vol_patt = re.compile("Cell volume\s*=\s*([\d.]+)")
+		maxforce_patt = re.compile("Max\s*([\d.-]+)")
+
+		s_line_patt = re.compile(r"siesta:\s*(.+)\s*=\s*([\d.-]+)\s*")
+
+		step_patt = re.compile(r"Begin \w+ move\s*=.*\*Maximum dynamic memory allocated", re.M)		
+
+		def get_var(content, name):
+			var = re.search(".*(" + name + ")" + "\s*[=|:]\s*([\w\d.-]+)", content, re.M)
+			if var:
+				return var.groups()[1]
+			else:
+				return None
+
+		#Getting chunk for a specific step 
+		def get_step(content, n):
+			search_str = "Begin \w+ move\s*=\s*" + str(n)
+			m = re.search(search_str, content, re.M)			
+			if m:
+				begin = m.start()
+				end = content.find("* Maximum dynamic memory allocated", begin)
+				if end != -1:
+					return content[begin:end]
+
+			return ""
+
+		def get_last_step(content):
+			begin = content.rfind("move =")
+			end = content.find("* Maximum dynamic memory allocated", begin)
+			if begin != end != -1:
+				return content[begin:end]
+			else: 
+				return ""
+
+
 		def find_by_pattern(pattern, str):
 			match = pattern.search(str)
 			if match:
@@ -272,32 +312,29 @@ class SiestaOutput(object):
 			else:
 				return ''
 
-		ver_patt = re.compile("Siesta Version\s*:\s*(.*)", re.I)
-		arc_patt = re.compile("Architecture\s*:\s*(.*)", re.I | re.M)
-		nspecies_patt = re.compile("Number of Atomic Species\s*=\s*(\d+)", re.I | re.M)
-		stype_patt = re.compile("System type\s*=\s*(\w+)", re.I | re.M)
-		natom_patt = re.compile("superc: Number of atoms, orbitals, and projectors\s*:\s*(\d+)\s+", re.I | re.M)
-		num_kpts_patt = re.compile("Number of k-points\s*=\s*(\d+)", re.I | re.M)
-		mesh_cutoff_patt = re.compile("Mesh cutoff \(required, used\)\s*=\s*(.+)Ry", re.I | re.M)
-		f_energy_patt = re.compile("Final energy (eV):", re.I | re.M)
-		qtot_patt = re.compile("Qtot\s*=\s*([\d.-]+)")
-		basisfun_patt = re.compile("nzeta=(\d+)\s+polorb=(\d+)")
-		press_patt = re.compile("([\d.]+)\s*([\d.]+)\s*Ry/Bohr\*\*3")
-		cell_vol_patt = re.compile("Cell volume\s*=\s*([\d.]+)")
-		maxforce_patt = re.compile("Max\s*([\d.-]+)")
+		# ver_patt = re.compile("Siesta Version\s*:\s*(.*)", re.I)
+		# arc_patt = re.compile("Architecture\s*:\s*(.*)", re.I | re.M)
+		# nspecies_patt = re.compile("Number of Atomic Species\s*=\s*(\d+)", re.I | re.M)
+		# stype_patt = re.compile("System type\s*=\s*(\w+)", re.I | re.M)
+		# !!natom_patt = re.compile("superc: Number of atoms, orbitals, and projectors\s*:\s*(\d+)\s+", re.I | re.M)
+		# !!num_kpts_patt = re.compile("Number of k-points\s*=\s*(\d+)", re.I | re.M)
+		# !!mesh_cutoff_patt = re.compile("Mesh cutoff \(required, used\)\s*=\s*(.+)Ry", re.I | re.M)
 
-		s_line_patt = re.compile(r"siesta:\s*(.+)\s*=\s*([\d.-]+)\s*")
+
 
 		with open(filename) as f:
 			content = f.read()
 
-			self.ver = find_by_pattern(ver_patt, content)
-			self.arc = find_by_pattern(arc_patt, content)
-			self.nspecies = int(find_by_pattern(nspecies_patt, content))
-			self.sys_type = find_by_pattern(stype_patt, content)
-			self.num_atoms = int(find_by_pattern(natom_patt, content))
-			self.num_kpts = int(find_by_pattern(num_kpts_patt, content))
-			self.mesh_cutoff = [float(mk) for mk in find_by_pattern(mesh_cutoff_patt, content).split()]
+			self.ver = get_var(content, "Siesta Version")
+			self.arch = get_var(content, "Architecture")
+			self.nspecies = int(get_var(content, "Number of Atomic Species"))
+			self.sys_type = get_var(content, "System type")
+
+			last_step = get_last_step(content)
+			self.num_kpts = int(get_var(last_step, "Number of k-points"))
+			self.num_steps = int(get_var(last_step, "Step"))
+			self.pressure = float(get_var(last_step, "Pressure \(total\)"))
+			print self.pressure
 
 			self.total_energy = False
 			self.charge = False
@@ -306,54 +343,10 @@ class SiestaOutput(object):
 			self.num_basis_func = -1
 			self.cell_vol = -1
 
-			#Parse Final Energy
-			fe_block = cut(content, 'Final energy (eV):', 'siesta: Stress tensor')
-			en_list = s_line_patt.findall(fe_block)
-			if en_list:
-				final_energy = {}
-				for en_type, value in en_list:
-					final_energy[en_type.strip()] = float(value)
-				if 'Total' in final_energy:
-					self.total_energy = final_energy['Total']
 			
-			#charge (in mulliken: spin up + spin down)
+			# for m in var_patt.finditer(content):
+			# 	print m.group()
 
-			########################33\
-			# No diff
 
-			up_down_charge = qtot_patt.findall(content)
-			if len(up_down_charge) == 2:
-				self.charge = float(up_down_charge[0]) + float(up_down_charge[1])
 
-			#number of basis functions
-			#TODO:
-			#for number of species
-			#NumberOfSpecies
-			#use AtomicCoordinatesAndAtomicSpecies
-			basis_block = cut(content, '<basis_specs>', '</basis_specs>')
-			basis_data = basisfun_patt.findall(basis_block)
-			if basis_data:
-				nfunc = 0
-				for nzeta, polarb in basis_data:
-					nfunc = nfunc + int(nzeta) + int(polarb)
-			if nfunc:
-				self.num_basis_func = nfunc * self.num_atoms
-
-			#pressure in Ry/Bohr**3
-			pressure = {}
-			press_block = cut(content, 'Pressure (static):', '(Free)E+ p_basis*V_orbitals')
-			press_data = press_patt.search(press_block)
-			if press_data:
-				self.pressure_solid = float(press_data.group(1))
-				self.pressure_mol = float(press_data.group(2))
-
-			#cell volume in Ang**3
-			cell_vol = cell_vol_patt.search(content)
-			if cell_vol:
-				self.cell_vol = float(cell_vol.group(1))
-
-			#max atomic force in eV/Ang
-			aforces_block = cut(content, 'Atomic forces (eV/Ang):', 'Stress-tensor-Voigt')
-			aforces_data = maxforce_patt.findall(aforces_block)
-			if aforces_data:
-				self.atomic_force = float(aforces_data[-1])
+elem = SiestaOutput("/home/artem/project/pymatgen/test_files/graphene_Sc_output")
